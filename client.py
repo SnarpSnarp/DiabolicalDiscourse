b257b8372c0dafd594b70d4c79a9682924936cc8
SnarpSnarp <andrewlewissm@gmail.com>
2023-06-19 06:35:52 -0400

    modified:   client.py

--- client.py
+++ client.py
@@ -8,3 +8,25 @@ customtkinter.set_default_color_theme("dark-blue")
 root = customtkinter.CTk()
 root.geometry("500x300")
 
+def login():
+    print("test")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System")
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login) 
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
